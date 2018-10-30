167763a76f05033d485eff02e2c21cf7b37a080b
Raniel Santos <bs.raniel@gmail.com>
2018-10-30 14:47:08 -0300

    adicionando estrutura inicial do projeto

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import ContentPage from './Components/ContentPage';
+
+class App extends Component {
+  render() {
+    return (
+      <fragment>
+
+        <Navbar></Navbar>
+        <ContentPage></ContentPage>
+        <Footer></Footer>
+
+      </fragment>
+      
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/ContentPage/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+
+const ContentPage = (props) =>{
+    return <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+    <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+      <div class="my-3 py-3">
+        <h2 class="display-5">Colocar Titulo aqui</h2>
+        <p class="lead">Escrever sobre algo e colocar imagens se necessario.</p>
+      </div>
+      <div class="bg-light box-shadow mx-auto content-div"></div>
+    </div>
+    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+      <div class="my-3 p-3">
+        <h2 class="display-5">Colocar Titulo aqui</h2>
+        <p class="lead">Escrever sobre algo e colocar imagens se necessario.</p>
+      </div>
+      <div class="bg-dark box-shadow mx-auto content-div"></div>
+    </div>
+  </div>
+};
+
+export default ContentPage;
