5f8bc23ccf4e7238964c54f53df023114c847473
Raniel Santos <bs.raniel@gmail.com>
2018-11-08 08:53:30 -0200

    Updates

---
+++ precache-manifest.66ff11ace9dd1623b234eaa450a102d6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "28af36475ef574f7563dbdd542d9a077",
+    "url": "/trabwebt02/static/media/Casamento.28af3647.PNG"
+  },
+  {
+    "revision": "bbbab54dd28388290ff7",
+    "url": "/trabwebt02/static/css/main.3cb6bdc3.chunk.css"
+  },
+  {
+    "revision": "492f714aca66b01875ac",
+    "url": "/trabwebt02/static/js/1.492f714a.chunk.js"
+  },
+  {
+    "revision": "75be2d136e2d599ca07f",
+    "url": "/trabwebt02/static/js/runtime~main.75be2d13.js"
+  },
+  {
+    "revision": "e530193b1aed84ef1ee76249df139826",
+    "url": "/trabwebt02/static/media/logo.e530193b.png"
+  },
+  {
+    "revision": "1a4328fb6a189799304015d4bd43046b",
+    "url": "/trabwebt02/static/media/profile.1a4328fb.jpg"
+  },
+  {
+    "revision": "6b42943dc403f41e91cf0b82b5defb45",
+    "url": "/trabwebt02/static/media/logo2.6b42943d.png"
+  },
+  {
+    "revision": "fcf6a61e5421a495506a423ef118a16c",
+    "url": "/trabwebt02/static/media/ff.fcf6a61e.gif"
+  },
+  {
+    "revision": "c347feacc5e971e050374efcbc7a0749",
+    "url": "/trabwebt02/static/media/Arani.c347feac.jpg"
+  },
+  {
+    "revision": "1908c6aa734b176cb5a4e032d1ec7a2f",
+    "url": "/trabwebt02/static/media/pacman.1908c6aa.jpg"
+  },
+  {
+    "revision": "b4533d5390ced8639f6be9f709057308",
+    "url": "/trabwebt02/static/media/rock.b4533d53.jpg"
+  },
+  {
+    "revision": "ac87be4a1572f19c009f27293e452138",
+    "url": "/trabwebt02/static/media/firstWeb.ac87be4a.PNG"
+  },
+  {
+    "revision": "bbbab54dd28388290ff7",
+    "url": "/trabwebt02/static/js/main.bbbab54d.chunk.js"
+  },
+  {
+    "revision": "8d648b247bd47908af87860f0cddbdcd",
+    "url": "/trabwebt02/static/media/Bootstrap1.8d648b24.PNG"
+  },
+  {
+    "revision": "52899c0fc340005073196aaf4f4e1f22",
+    "url": "/trabwebt02/static/media/svg.52899c0f.PNG"
+  },
+  {
+    "revision": "09192d1a651e946d7e8c875ae81fd489",
+    "url": "/trabwebt02/static/media/scrumgame.09192d1a.jpg"
+  },
+  {
+    "revision": "c0bfa1fac5816d28460316e628e12135",
+    "url": "/trabwebt02/static/media/shield.c0bfa1fa.jpg"
+  },
+  {
+    "revision": "0621c1763293442255862bc30daf4d77",
+    "url": "/trabwebt02/static/media/ff2.0621c176.gif"
+  },
+  {
+    "revision": "e14d52bbfa90dabdcb195713a43e0865",
+    "url": "/trabwebt02/static/media/ff3.e14d52bb.gif"
+  },
+  {
+    "revision": "6a77980c9ed704331a259833a65f7e9e",
+    "url": "/trabwebt02/static/media/facebook.6a77980c.png"
+  },
+  {
+    "revision": "a0b10c327ae74db607dca6929b3c22fb",
+    "url": "/trabwebt02/static/media/github.a0b10c32.png"
+  },
+  {
+    "revision": "2a88b361cc04b8fc29d1abad3a7ffc58",
+    "url": "/trabwebt02/static/media/instagran.2a88b361.png"
+  },
+  {
+    "revision": "115f3eec0cd296d666cac0f3706f66ca",
+    "url": "/trabwebt02/static/media/bckControl.115f3eec.png"
+  },
+  {
+    "revision": "2f1c570c1dedebfb810b1355bafc0de9",
+    "url": "/trabwebt02/index.html"
+  }
+];
